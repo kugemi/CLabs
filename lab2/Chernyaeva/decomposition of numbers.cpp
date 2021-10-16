3ff2253eac3724236fb5e6e8c438a47882d90aaf
= <=>
2021-10-17 00:27:13 +0300

    initial commit

---
+++ lab2/Chernyaeva/decomposition of numbers.cpp
@@ -0,0 +1,37 @@
+﻿/*простой состав числа*/
+#include<stdio.h>
+#include<conio.h>
+#include<locale.h>
+int checkingTheNumber(long summand);
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int number;
+	int firstSummand;
+	int secondSummand;
+	printf("Введите четное число: ");
+	while ((scanf_s("%d", &number) == 0) || (number < 0) || (number % 2 != 0))
+	{
+		printf("Неверное полученное значение, попробуйте еще раз: ");
+		while (getchar() != '\n');
+	}
+	printf("\n");
+	for (firstSummand = 1; firstSummand <= (number / 2); firstSummand++)
+		if (checkingTheNumber(firstSummand))
+			for (secondSummand = 1; secondSummand <=(number-firstSummand); secondSummand++)
+				if (checkingTheNumber(secondSummand))
+					if ((firstSummand + secondSummand) == number)
+						printf(" %d = %d + %d \n",number, firstSummand, secondSummand);
+	_getch();
+
+}
+int checkingTheNumber(long summand)
+{
+	int divider;
+	if (summand < 4) return 1;
+	if (summand % 2 == 0) return 0;
+	for (divider = 3; divider * divider <= summand; divider += 2)
+		if (summand % divider == 0) return 0;
+	return 1;
+}
---
+++ lab4/Chernyaeva/guess the number(1).cpp
@@ -0,0 +1,63 @@
+﻿/*угадайка:)*/
+#include<stdio.h>
+#include<conio.h>
+#include<locale.h>
+#include<stdlib.h>
+#include<time.h>
+#include<ctype.h>
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	int enteredNumber;
+	int randomNumber;
+	int numberOfAttempts;
+	int i;
+	char n='Y';
+	srand(time(0));
+	while (n=='Y')
+	{
+		randomNumber = (rand() % 100) + 1;
+		printf("Введите количество попыток: \n");
+		while (((scanf_s("%d", &numberOfAttempts)) != 1) || (numberOfAttempts) <= 0)
+		{
+			printf("неверно введенное число, попробуйте снова: \n");
+			while (getchar() != '\n');
+		}
+		printf("Введите число: ");
+		for (i = 0; i < numberOfAttempts; i++)
+		{
+			while ((scanf_s("%d", &enteredNumber)) == 0) {
+				printf("\nНеверно введенное значение, попробуйте снова: ");
+				while (getchar() != '\n');
+			}
+			if (enteredNumber == randomNumber)
+			{
+				printf("\nВы угадали!\nКоличество использованных попыток: %d", i + 1);
+				printf("\n\nХотите продолжить игру(Y/N)?\n");
+				n =_getch();
+				n = toupper(n);
+				printf("\n");
+				_getche();
+				break;
+			}
+			if (i == (numberOfAttempts - 1))
+			{
+				printf("\nПопытки кончились, вы не угадали:(");
+				printf("\nЗагаданное число-%d", randomNumber);
+				printf("\n\nХотите продолжить игру(Y/N)?\n");
+				n = _getch();
+				n = toupper(n);
+				printf("\n");
+				_getche();
+				break;
+			}
+			if (enteredNumber > randomNumber)
+				printf("\nВведенное число больше, попробуйте снова:  ");
+			if (enteredNumber < randomNumber)
+				printf("\nВведенное число меньше, попробуйте снова:  ");
+
+
+		}
+	}
+}
