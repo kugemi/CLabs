b82c9cf1f17c6607d3c757b70ae1ef0d4e8c5268
= <=>
2021-10-21 13:18:00 +0300

    fixed

--- lab5/Chernyadeva A.N/Laba5.cpp
+++ lab5/Chernyadeva A.N/Laba5.cpp
@@ -1,28 +1,29 @@
 ﻿#include <stdio.h>
 #include <conio.h>
 #include <locale.h>
+
+
+
 #define lineBreak printf("\n")
+#define symbolCheckAND(cChar) ((cChar != ' ') && (cChar != '\n') && (cChar != '\t')  && (cChar != '.') && (cChar != ',') && (cChar != '?') && (cChar != '!'))
+#define symbolCheckOR(cChar) ((cChar == ' ') || (cChar == '\n') || (cChar == '\t') || (cChar == '.') || (cChar == '!') || (cChar == '?'))
 
-
-//он не считает последнее слово!
-// 
-// энтер и ctrl + z, после текста обязательно сделать пробел.
 int main() {
-	unsigned nSymbol;                      // Кол-во символов
-	unsigned nSentences;                   // Кол-во предложений
-	unsigned nWordsinText;                 // Количество слов во всём тексте
-	unsigned nWordsinSent;                 // Количество слов в одном предложении
+
+	// Объявление и инициализация перемнных
+	unsigned nSymbol = 0;                      // Кол-во символов
+	unsigned nSentences = 0;                   // Кол-во предложений
+	unsigned nWordsinText = 0;                 // Количество слов во всём тексте
+	unsigned nWordsinSent = 0;                 // Количество слов в одном предложении
 	char cChar;                            // Считываемая буква
-	char flag = 1;                         // Флаг на выход из числа
+	//char flag = 1;                         // Флаг на выход из числа
 	int i, j;                              // Счётчики
 	int WordBarGraph[100] = { 0 };         // Массив для хранения кол-ва символов в словах
 	int SentencesBarGraph[100] = { 0 };    // Массив для храниения количества слов в предложениях.
-	int FullCell;                          // Считает кол-во непустых ячеек в массиве SentencesBarGraph
-	int maxCBG;                            // Принимает максимальное из значений массива 
+	int FullCell = 0;                          // Считает кол-во непустых ячеек в массиве SentencesBarGraph
+	int maxCBG = 0;                            // Принимает максимальное из значений массива 
 
-
-	// Инициализация переменных
-	nSymbol = nSentences = nWordsinText = nWordsinSent = FullCell = maxCBG = 0;
+	printf("");
 
 	// Подключение русского языка
 	setlocale(LC_ALL, "Rus");
@@ -50,34 +51,28 @@ int main() {
 	
 	printf("Начинайте вводить текст.\n\n <<|");
 
-
-
-
 	// Считывание символов.
 	while ((cChar = getchar()) != EOF)
 	{
-		// Проверка на конец слова. Если конец, счётчик слов уеличивается, в массив запоминается кол-во символов в слове.
-		if (cChar == ' ' || cChar == '\n' || cChar == '\t')
+
+		// Проверка на конец слова. Если конец, счётчик слов увеличивается, в массив запоминается кол-во символов в слове.
+		if (symbolCheckOR(cChar) && (nSymbol != 0))
 		{
-			flag = -1;
-		}
-		else if (flag == -1)
-		{
-			flag = 1;
-			WordBarGraph[++nWordsinText] = nSymbol-1;
+			WordBarGraph[nWordsinText] = nSymbol;
+			nWordsinText++;
 			nWordsinSent++;
-			nSymbol = 0;
+			nSymbol = 0;			
 		}
 
 		// Проверка на конец предложения. Если конец, счётчик предложений увеличивается, в массив запоминается кол-во слов.
-		if (cChar == '.' || cChar == '!' || cChar == '?')
+		if ((cChar == '.') || (cChar == '!') || (cChar == '?'))
 		{
-			nSentences++;
 			SentencesBarGraph[nSentences] = nWordsinSent;
+			nSentences++;
 			nWordsinSent = 0;
-			
+
 		}
-		nSymbol++;
+		if (symbolCheckAND(cChar)) nSymbol++;
 	}
 	printf("|>>\n\n\n");
 
@@ -85,15 +80,13 @@ int main() {
 	printf("= = = = =   Результат работы программы:   = = = = =\n\n");
 
 
-	// Первое слово программа не считывает, поэтому для верности добавляется 1.
-	SentencesBarGraph[1]++;
-
 	// Счёт непустых ячеек в массиве для того, чтобы потом построить вертикальную гистограмму.
 	for (i = 0; i < 30; i++) if (SentencesBarGraph[i] != 0) FullCell++;
 
-	//
-	//////////////////////Вывод ответов и гистограмм
-	//
+
+	/*
+	------------------Вывод ответов и гистограмм
+	*/
 
 	// Вывод количества слов в тексте + склонение по кол-ву.
 
@@ -102,34 +95,36 @@ int main() {
 	else if (nWordsinText%10 > 1 && nWordsinText%10 < 5) printf("слова\n");
 	else printf("слов\n");
 
+
 	printf("\n\n--------------   График слов и букв в словах.   --------------\n");
-	printf(" \\ --> Кол - во символов\n");
+	printf(" \\ --> Кол-во символов\n");
 	printf("|\n");
 	printf("V\n");
-	printf("Кол-во слов\n\n");
+	printf("Кол-во\nслов\n\n");
+
 	// Вывод кол-ва символов по словами в виде горизонтальной гистограммы.
 
-	//printf("|   ");
-	for (i = 1; i < 100; i++)
+	for (i = 0; i < 100; i++)
 	{
-		printf("|   ");
 		if (WordBarGraph[i] == 0) break;
+		printf("|   ");
 		for (j = 0; j < WordBarGraph[i]; j++) printf("+");
 		if (j == WordBarGraph[i])
 		{
 			lineBreak;
 		}
 	}
+
 	// Вычисление максимального значения в массиве SentencesBarGraph
 	for (i = 0; i < 100; i++) if (SentencesBarGraph[i] > maxCBG) maxCBG = SentencesBarGraph[i];
 
 	printf("\n\n-------   График предложений и слов в предложениях.   -------\n\n");
-	
+
 	// Вывод кол-ва слов по предложениям в виде вертикальной гистограммы.
 	for (j = maxCBG; j > 0; j--)
 	{
 		printf("|   ");
-		for (i = 1; i <= FullCell; i++)
+		for (i = 0; i <= FullCell; i++)
 		{
 			if (SentencesBarGraph[i] == j)
 			{
@@ -140,10 +135,11 @@ int main() {
 		}
 		lineBreak;
 	}
-	printf("\nКол-во слов\n");
+	printf("\nКол-во\nслов\n");
 	printf("^\n");
 	printf("|\n");
-	printf("/--> Кол - во предложений\n");
+	printf("/--> Кол-во предложений\n");
 	printf("=====================================================================\n");
 	for (i = 0; i < 30; i++) printf(" ");
-	printf("Программа завершена!\n\n\n\n");}
+	printf("Программа завершена!\n\n\n\n");
+}
