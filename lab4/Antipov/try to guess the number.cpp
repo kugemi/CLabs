759d46a07d4d5e151185d7616791a7c8b77adb76
= <=>
2021-10-25 08:44:32 +0300

    fixed

--- lab4/Antipov/try to guess the number.cpp
+++ lab4/Antipov/try to guess the number.cpp
@@ -1,47 +1,49 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 #include <locale.h>
 #include <conio.h>
+#include<ctype.h>
 
 
-void main()
+int main()
 {
 	int min = 0;
 	int number;
-	char answer = 'a';
-	int attempt = 1;
+	char answer = 'Y';
+	int attempt;
+	int index;
 	int variant = 0;
 	setlocale(LC_ALL, "Russian");
 	printf("Я хочу сыграть с тобой в игру, хочешь ли ты этого? (y/n):");
-	  do {
+	do {
 		answer = getchar();
-		if ((answer != 'y') && (answer != 'n')) { printf(" Сейчас не понял\n"); }
+		if (((answer != 'Y') && (answer != 'N')) && ((answer != 'y') && (answer != 'n'))) { printf(" Сейчас не понял\n"); }
 		while (getchar() != '\n');
-	 } while ((answer != 'y') && (answer != 'n'));
+	} while (((answer != 'Y') && (answer != 'N')) && ((answer != 'y') && (answer != 'n')));
 
-	 while (answer == 'y') {
-		 srand(time(0));
-		 number = min + rand() % (100+1);
-		 printf("Чисто по секрету загаданное число( %i)\n", number);
-		 for (attempt = 1; attempt < 11; attempt++) {
-			 printf("%i: Введите число: ", attempt);
-			 scanf_s("%i", &variant);
-			 if (number > variant) { printf("Загаданное число побольше\n"); }
-			 if (number < variant) { printf("Загаданное число поменьше\n"); }
-			 if (number == variant) { printf("Ты угадал\nПопыток использовано: %i\n ", attempt); break; }
-			 if (number != variant && attempt == 10) { printf("Неудачник, твои попытки кончились.Загаданное число= %i\n", number); }
-		 }
-		 printf("Продолжим нашу игру?(y/n):");
-		 do {
-			 scanf_s(" %c", &answer);
-			 if ((answer != 'y') && (answer != 'n')) { printf(" Сейчас не понял\n"); }
-			 while (getchar() != '\n');
-		 } while ((answer != 'y') && (answer != 'n'));
+	while ((answer == 'Y') || (answer == 'y')) {
+		printf("Введите количество попыток = ");
+		scanf_s("%i", &attempt);
+		srand(time(0));
+		number = min + rand() % (100 + 1);
+		printf("Чисто по секрету загаданное число( %i)\n", number);
+		for (index = 1; index < attempt + 1; index++) {
+			printf("%i: Введите число: ", index);
+			scanf_s("%i", &variant);
+			if (number > variant) { printf("Загаданное число побольше\n"); }
+			if (number < variant) { printf("Загаданное число поменьше\n"); }
+			if (number == variant) { printf("Ты угадал\nПопыток использовано: %i\n ", index); break; }
+			if (number != variant && index == attempt) { printf("Неудачник, твои попытки кончились.Загаданное число =  %i\n", number); }
+		}
+		printf("Продолжим нашу игру?(y/n):");
+		do {
+			scanf_s(" %c", &answer);
+			if (((answer != 'Y') && (answer != 'N')) && ((answer != 'y') && (answer != 'n'))) { printf(" Сейчас не понял\n"); }
+			while (getchar() != '\n');
+		} while (((answer != 'Y') && (answer != 'N')) && ((answer != 'y') && (answer != 'n')));
 
-	 }
-	 
-	 _getch();
+	}
+	_getch();
 }
