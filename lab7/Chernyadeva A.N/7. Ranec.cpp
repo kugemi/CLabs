6bfd50717b1c715348c7f55cf3b27683b215d88d
= <=>
2021-10-31 23:08:08 +0300

    Редач лабы дубль 1

--- lab7/Chernyadeva A.N/7. Ranec.cpp
+++ lab7/Chernyadeva A.N/7. Ranec.cpp
@@ -14,7 +14,7 @@ void main()
 	// Константы
 	const int max_items = 10;             // Ограничение кол-ва предметов
 	const int max = 1024;                 // Ограничение массивов
-	const int k = 70;                     // Длина строки для сообщений
+	const int k = 70;
 
 	 //        Переменные, вводимые с клавиатуры
 	unsigned int itemNumber = 0;          // Количество предметов 
@@ -26,15 +26,13 @@ void main()
 	unsigned int runUsefull = 0;          // Текущая полезность предметов в переборе
 	unsigned int runWeight = 0;           // Текущий вес предметов в переборе
 	unsigned int maxUsefull = 0;          // Максимальная полезность вмещающихся в ранец предметов
-	unsigned int maxWeight = 0;
-	unsigned int runPosition[max] = { 0 };// Текущие позиции предметов, которые мы просматриваем
-	unsigned int Position[max] = { 0 };   // Позиции предметов, которые точно будут в ранце
+	unsigned int maxWeight = 0;           // Максимальный вес вмещающихся в ранец предметов
+	unsigned int numberBruteforse = 0;    // Номер перебора с ответом
+	unsigned int mask = 1;                // Маска для выборки
 
 	//         Счётчики
 	int index = 0;                        // Счётчик
-	int Jndex = 0;                        // Счётчик, дублирующий index в цикле while для его изменения
-	int numberIteration = 0;              // Счётчик числа итераций в цикле while
-	int indexZeroing = 0;                 // Счётчик для обнуления массива в цикле while
+	int Jndex = 0;                        // Счётчик итераций
 
 	//Приветственное сообщение, ввод данных
 	lineBreak;
@@ -55,7 +53,7 @@ void main()
 	lineBreak;
 
 	//Ввод и проверка кол-ва предметов
-	Space(k); printf("  | Введите количество предметов, которое бы Вы\n");
+	Space(k);; printf("  | Введите количество предметов, которое бы Вы\n");
 	Space(k); printf("  | хотели поместить в ранец.\n");
 	Space(k); printf("  | Кол-во не должно превышать %d\n", max_items);
 	Space(k); printf(">>| ");
@@ -109,46 +107,50 @@ void main()
 	printf(" | Вместимость ранца: %d\n", briefcaseCapacity);
 	printf(" |----------------------------------------------------------------------\n");
 
-	//Перебор всех возможных вариантов 2^кол-во предметов
 	for (index = 0; index < pow(2, itemNumber); index++)
 	{
-		Jndex = index;   // Дублируем номер перебора, чтобы далее взаимодействовать с ним
-		numberIteration = 0;       // Обнуление счётчика числа итераций в while
+		mask = 1;       //"Обнуление" маски, присваивание изначального значения
 		runUsefull = 0; // Обнуление текущей полезности
 		runWeight = 0;  // Обнуление текущего веса
-		// Обнуление текущего массива номеров предметов
-		for (indexZeroing = 0; indexZeroing < itemNumber; indexZeroing++) runPosition[indexZeroing] = 0;
-
-		// Осуществляется перебор по битам номера перебора index 
-		while (Jndex >= 1)
+		
+		// Осуществляется перебор по битам index 
+		for (Jndex = 0; Jndex < itemNumber; Jndex++)
 		{
-			if (Jndex % 2 == 1)                   //Если бит на месте есть...
+			if (mask & index) //Если бит на месте есть...
 			{
-				runUsefull += itemUsefull[numberIteration];   // ... то суммируется полезность и вес предмета на месте кол-ва итераций g
-				runWeight += itemWeight[numberIteration];
-				runPosition[numberIteration]++;                 // в массив сохраняется бит - порядковый номер добавленного в ранец предмета
+				runUsefull += itemUsefull[Jndex];   // ... то суммируется полезность и вес предмета на месте кол-ва итераций Jndex
+				runWeight += itemWeight[Jndex];
 			}
-			numberIteration++;  // увеличиваем кол-во итераций
-			Jndex /= 2;  //Переходим к следующему биту числа index 
+			mask <<= 1; 
+			//printf("\\\ni = %d\nJndex = %d\nmask = %d\nrunUsefull = %d\nrunWeight = %d\nto_samoe = %d\n\n", index, Jndex, mask, runUsefull, runWeight, to_samoe);
+		}
+		// Если текущий вес меньше вместимости ранца И текущая полезность выше максимальной...
+		if ((runWeight <= briefcaseCapacity) && (runUsefull >= maxUsefull))
+		{
+			// ... то в максимальную полезность (оптимум) записывается текущая,
+			maxUsefull = runUsefull;
+			// в максимальный вес записывается текущее.
+			maxWeight = runWeight;
+			// в номер "той самой" итерации сохраняется значение index
+			numberBruteforse = index;
 		}
 		
-		// Если текущий вес меньше вместимости ранца И текущая полезность выше максимальной...
-		if ( (runWeight <= briefcaseCapacity) && (runUsefull >= maxUsefull))
-		{
-			// ... то в максимальную полезность (оптимум) записывается текущая.
-			maxUsefull = runUsefull;
-			maxWeight = runWeight;
-			//В отдельный массив сохраняются позиции предметов, которые точно надо положить в ранец
-			for (indexZeroing = 0; indexZeroing < pow(2, itemNumber); indexZeroing++) Position[indexZeroing] = runPosition[indexZeroing];
-		}
-
 	}
-
 	// Вывод решения
 	lineBreak;
 	Space(k); printf("  | Вам стоит взять с собой предметы:\n");
 	Space(k); printf("  | ");
-	for (index = 0; index <= itemNumber; index++) if (Position[index] == 1) printf("#%d   ", index + 1);
+
+	mask = 1; // Присваивание изначального значения
+
+	//Пробегаемся по битам числа numberBruteforse и выводим номера предметов
+	for (index = 0; index <= itemNumber; index++)
+	{
+		if (mask & numberBruteforse) printf("#%d   ", index + 1);
+		mask <<= 1;
+	}
+
+
 	lineBreak;
 	Space(k); printf("  ------------------------------------\n");
 	Space(k); printf("  | Полезность предметов = %d\n", maxUsefull);
