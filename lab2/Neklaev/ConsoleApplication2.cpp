10b353938677a439aaf67fd2c5a2485773b08ae8
0Easygame0 <danil.neklaev@mail.ru>
2021-11-01 13:15:33 +0400

    initial commit

---
+++ lab2/Neklaev/ConsoleApplication2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <locale.h>
+#include <conio.h>
+#include<ctype.h>
+
+
+int main()
+{
+	char otvet = 'Y';
+	int popitka, chislo, index, variant;
+	setlocale(LC_ALL, "Russian");
+	printf("Игра начинается\n");
+	while ((otvet == 'Y') || (otvet == 'y')) {
+		printf("Введите количество попыток = ");
+		scanf_s("%i", &popitka);
+		srand(time(0));
+		chislo = 0 + rand() % (100 + 1);
+		for (index = 1; index < popitka + 1; index++) {
+			printf("%i: Угадайте число: ", index);
+			scanf_s("%i", &variant);
+			if (chislo > variant) { printf("Загаданное число больше введённого\n"); }
+			if (chislo < variant) { printf("Загаданное число меньше введённого\n"); }
+			if (chislo == variant) { printf("Ты угадал\nПопыток использовано: %i\n ", index); break; }
+			if (chislo != variant && index == popitka) { printf("Попытки закончились. Загаданное число =  %i\n", chislo); }
+		}
+	printf("Сыграем ещё раз?(y/n):");
+	do {
+		scanf_s(" %i", &otvet);
+		otvet = getchar();
+		if ((otvet != 'Y') && (otvet != 'N') && (otvet != 'y') && (otvet != 'n')) { printf(" Недопустимый ответ\n"); }
+		while (getchar() != '\n');
+	} while (((otvet != 'Y') && (otvet != 'N')) && ((otvet != 'y') && (otvet != 'n')));
+	
+
+	}
+	_getch();
+}
