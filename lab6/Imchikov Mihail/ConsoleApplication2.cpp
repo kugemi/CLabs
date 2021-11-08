01ca12f7c0384eadcfd19ebb4e74048d25376ff5
Михаил Ямщиков <=>
2021-11-08 10:52:03 +0300

    2 blin

--- lab6/Imchikov Mihail/ConsoleApplication2.cpp
+++ lab6/Imchikov Mihail/ConsoleApplication2.cpp
@@ -6,22 +6,20 @@
 
 int main()
 {
-	unsigned char num=0;
 	unsigned char num2=0; // эта переменная для действий 
 	unsigned char maskR=1;// маска права
 	unsigned char maskL = 128;// маска лево
 	char answer= 'o';
 	int num1=300;// нужен для записи 
 	int shift=0;// сдвиг
-	int remains[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };// остаток при делении
 	while (num1 > 255 || num1 < 0) 
 	{
 		printf("Enter a number from 0 to 255 ");
 		scanf_s("%i", &num1);
 	}
-	num = (unsigned char)num1;
+	num1 = (unsigned char)num1;
 	while (answer != 'n') {
-		num2 = num;                        
+		num2 = num1;
 		if (answer != 'o')
 		{
 			printf("For how many positions?\n");
@@ -38,13 +36,14 @@ int main()
 			}
 		}
 		printf("%i = ", num2);
-		for (int i = 0; i < 8; i++) {
-			remains[i] = num2 % 2;
-			num2 = num2 / 2;
+		if (num2 & maskL) printf("1");
+		else printf("0");
+		for (int i = 0; i <7; i++) {
+			num2 = num2 << 1;
+			if (num2 & maskL) printf("1");
+			else printf("0");
 		}
-		for (int i = 7; i > -1; i--) { printf("%i", remains[i]); }
 		printf("\n");
-		
 		do {
 			printf("where do you want to shift left(l) to right(r) or do you want to finish(n)?\n");
 			scanf_s(" %c", &answer);
