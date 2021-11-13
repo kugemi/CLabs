5b6b4197a3561baa6c2b6f2ee8d0b19408d70e49
= <=>
2021-11-13 19:14:29 +0300

    initial commit

---
+++ lab6/Hohrin/laba6.cpp
@@ -0,0 +1,48 @@
+﻿#include <conio.h>
+#include <stdio.h>
+#include <locale.h>
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+	int input, shiftRight, shiftLeft, binaryInput, counter, counterRight, counterLeft, binary;
+	input = shiftRight = shiftLeft = counter = binaryInput = counterRight = counterLeft = 1;
+	binary = 2;
+	int binaryArray[100] = { 0 };
+	int binaryReverse[100] = { 0 };
+	printf("Введите число:");
+	scanf_s("%d", &input);
+	printf("Введите число сдвигов влево:");
+	scanf_s("%d", &shiftLeft);
+	printf("Введите число сдвигов вправо:");
+	scanf_s("%d", &shiftRight);
+	while (input > 0)
+	{
+		binaryReverse[counter] = input % 2;
+		input = input / 2;
+		counter++;
+	}
+	counter--;
+	for (int i = 0; i < counter; i++)
+	{
+		binaryArray[i] = binaryReverse[counter - i];
+	}
+	printf("Двоичная запись числа:");
+	for (int i = 0; i < counter; i++)
+	{
+		printf("%d", binaryArray[i]);
+	}
+	printf("\n Двоичная запись числа с сдвигом влево:");
+	for (int i = 0; i < counter; i++)
+	{
+		printf("%d", binaryArray[(i + shiftLeft) % counter]);
+	}
+
+	printf("\nДвоичная запись числа с сдвигом вправо:");
+	for (int i = 0; i < counter; i++)
+	{
+		printf("%d", binaryArray[((abs(counter*shiftRight) + i - shiftRight) % counter)]);
+	}
+
+}
---
+++ lab7/Hohrin/laba7.cpp
@@ -0,0 +1,60 @@
+﻿#include <conio.h>
+#include <stdio.h>
+#include <locale.h>
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "russian");
+    int maxWeight, max, amount, sum, input, weight = 0;
+    int counter = 1;
+    sum = 0;
+    max = 0;
+    int arrayWeight[100], arrayPrice[100], binaryReverse[1000], binaryArray[1000] = { 0 };
+    printf("Введите вместимость ранца:");
+    scanf_s("%d", &maxWeight);
+    printf("Введите количество вещей:");
+    scanf_s("%d", &amount);
+    for (int i = 0; i < amount; i++)
+    {
+        printf("Введите вес и стоимость предмета:");
+        scanf_s("%d %d", &arrayWeight[i], &arrayPrice[i]);
+    }
+    input = pow(2, amount) - 1;
+    for (int a = 0; a < pow(2, amount + 1); a++)
+    {
+        sum = 0;
+        weight = 0;
+        counter = 1;
+        while (input > 0)
+        {
+            binaryReverse[counter] = input % 2;
+            input = input / 2;
+            counter++;
+        }
+        counter--;
+        for (int b = 0; b < counter; b++)
+        {
+            binaryArray[b] = binaryReverse[counter - b];
+        }
+        if (input == 0)
+        {
+            for (int c = 0; c < 50; c++)
+            {
+                if (binaryArray[c] == 1)
+                {
+                    sum += arrayPrice[c];
+                    weight += arrayWeight[c];
+                }
+                if (weight <= maxWeight)
+                {
+                    if (sum > max)
+                    {
+                        max = sum;
+                    }
+                }
+            }
+        }
+    }
+    printf("максимальная ценность:%d", max);
+}
